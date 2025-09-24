a169669ab79dc7b47369cf7dcaef94b2ad65a1a4
mohamed-yehia311 <mmoa311311@gmail.com>
2025-09-24 21:23:06 +0300

    feat: implement BaseModel with database client and settings initialization

--- src/models/BaseModel.py
+++ src/models/BaseModel.py
@@ -1,1 +1,8 @@
 # The base Model for all database models
+from ..helpers.config import get_Settings, Settings
+
+class BaseModel:
+
+    def __init__(self, db_client: object):
+        self.db_client = db_client
+        self.app_settings = get_Settings()
