f44d6a133d8e39cd669e059aed09b9305d58998f
mohamed-yehia311 <mmoa311311@gmail.com>
2025-09-25 20:08:20 +0300

    feat: add RecommendationModel and Recommendation schema for managing recommendations

---
+++ src/models/db_schemas/Recommendation.py
@@ -0,0 +1,32 @@
+from typing import Optional, List
+from pydantic import BaseModel, Field, ValidationError, model_validator
+from bson import ObjectId
+from datetime import datetime, timezone
+
+
+class Recommendation(BaseModel):
+    id: Optional[ObjectId] = Field(None, alias="_id")
+
+    title: str = Field(..., min_length= 10, max_length=100, description="title of the recommendation filled by Ai agent")
+
+    content: str= Field(..., min_length=10, description="AI prompt to make a recommendation")
+
+    summary: str= Field(..., min_length=10, description="summary of Ai agent results")
+
+    result: str=Field(..., min_length=10, description="Recommendation results")
+
+    createdAt: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    
+    user_id: ObjectId
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+
+    @classmethod
+    def get_indexes(cls):
+        return [
+            {"key": [("user_id", 1)], "name": "user_index", "unique": False},
+            {"key":[("createdAt", -1)], "name":"createdAt", "unique": False}
+        ]
