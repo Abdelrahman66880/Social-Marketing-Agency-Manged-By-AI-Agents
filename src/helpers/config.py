9ef4e46dfccb9cef9995d1304bd3b97af7bbe20d
mohamed-yehia311 <mmoa311311@gmail.com>
2025-09-27 01:14:57 +0300

    feat: resolve errors

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,15 +1,17 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+from pydantic_settings import BaseSettings
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))  # points to src/
 
 class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
-
     MONGODB_URL: str
+    MONGODB_DATABASE: str
     MONGODB_DATABASE: str
 
     class Config:
-        env_file = ".env"
-
+        env_file = os.path.join(BASE_DIR, ".env")  # src/.env
 
 def get_Settings():
     return Settings()
