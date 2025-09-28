3c393e6cdc4a1e31311bba4fe3471b061063c92c
mohamed-yehia311 <mmoa311311@gmail.com>
2025-09-28 21:17:34 +0300

    feat: add a weebhook endpoint and include facebook route

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from src.routes import facebook
 app = FastAPI()
 
-@app.get("/")
-async def base_route():
-    return {"message": "Hello, World!"}
+VERIFY_TOKEN = "my_secret_123"  
+
+app.include_router(facebook.router)
+
+@app.get("/webhook")
+async def verify_webhook(request: Request):
+    mode = request.query_params.get("hub.mode")
+    token = request.query_params.get("hub.verify_token")
+    challenge = request.query_params.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return int(challenge)
+    return {"error": "Verification failed"}
+
+
+@app.post("/webhook")
+async def receive_message(request: Request):
+    data = await request.json()
+    print("Webhook event:", data)
+    return {"status": "ok"}
