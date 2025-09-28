b67c6fc1db9470e02b41067c8bfde827ceeeb7dd
Abdelrahman66880 <abdofathi317@gmail.com>
2025-09-28 05:54:19 +0300

    feat(api): add base router with welcome endpoint and settings retrieval

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,1 +1,21 @@
 # The base route for the application
+from fastapi import FastAPI, APIRouter, Depends
+from helpers.config import get_Settings, Settings
+import os
+
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"]
+)
+
+
+
+@base_router.get("/")
+async def welcome(app_setting: Settings = Depends(get_Settings)):
+    app_setting = get_Settings()
+    app_name = app_setting.APP_NAME
+    app_version = app_setting.APP_VERSION
+    return {
+        "app_name": app_name,
+        "app_version": app_version
+    }
