d2b723f919d014754794986021c614120e6a36a3
Abdelrahman66880 <abdofathi317@gmail.com>
2025-09-29 04:24:00 +0300

    update: .env file

--- src/models/schemas/postupdateSchams.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional
-
-class PageUpdateSchema(BaseModel):
-    about: Optional[str] = None
-    description: Optional[str] = None
-    email: Optional[EmailStr] = None
-    website: Optional[str] = None
