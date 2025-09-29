a1341336deafb0909661b1b5977f740adffe172c
Abdelrahman66880 <abdofathi317@gmail.com>
2025-09-29 03:30:44 +0300

    fix: update PageInfoSchema fields to be optional

--- src/models/schemas/postSchams.py
+++ src/models/schemas/postSchams.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional, List
 
 class PageInfoSchema(BaseModel):
-    id: str         #READ only
-    name: str       #READ only
+    id: Optional[str] = None         #READ only
+    name: Optional[str] = None       #READ only
     category: Optional[str] = None
     about: Optional[str] = None
     description: Optional[str] = None
