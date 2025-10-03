576457af075a8435969d06ddb51d8ee89aedcfbb
Abdelrahman66880 <abdofathi317@gmail.com>
2025-10-04 02:23:05 +0300

    initall commit

--- src/routes/analytics.py
+++ src/routes/analytics.py
@@ -3,3 +3,4 @@ from fastapi import APIRouter, status, Depends
 from typing import Optional, List
 from facebook import FacebookController
 from analytics import AnalyticsController
+from models import AnalyticsData, FacebookData
