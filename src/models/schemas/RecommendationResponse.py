839c946f152345823607e83098b75624b4139519
Abdelrahman66880 <abdofathi317@gmail.com>
2025-10-06 00:25:25 +0300

    fix: import error

--- src/models/schemas/RecommendationResponse.py
+++ src/models/schemas/RecommendationResponse.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
 from datetime import datetime
-
+from .Recommendation import Recommendation
 
 class RecommendationsResponse(BaseModel):
     """
