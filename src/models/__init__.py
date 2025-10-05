30c04b0589ad8b6b9ad756ada466475df73aaa3f
Abdelrahman66880 <abdofathi317@gmail.com>
2025-10-06 02:44:30 +0300

    fix: type error in __init__ file

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,7 +2,7 @@ from .schemas import InteractionsResponse
 from .schemas import facebookSchemas
 from .schemas import CompetitorRequest
 from .schemas import CompetitorSummary
-from controllers.analytics import AnalyticsController
+from src.controllers.analytics import AnalyticsController
 from .schemas.Recommendation import Recommendation
-from .schemas.RecommendationResponse import RecommendationResponse
+from .schemas.RecommendationsResponse import RecommendationsResponse
 from .schemas.RecommendationsRequest import RecommendationsRequest
