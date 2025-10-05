8f6758b8bb6aa71ec6b9a1dca4812dffff6a4f84
Abdelrahman66880 <abdofathi317@gmail.com>
2025-10-06 00:47:53 +0300

    feat: update the inint file

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,6 @@ from .schemas import facebookSchemas
 from .schemas import CompetitorRequest
 from .schemas import CompetitorSummary
 from controllers.analytics import AnalyticsController
+from .schemas.Recommendation import Recommendation
+from .schemas.RecommendationResponse import RecommendationResponse
+from .schemas.RecommendationsRequest import RecommendationsRequest
