0118a83ba55c9d0a06489f718e7aa4975c3a0169
Abdelrahman66880 <abdofathi317@gmail.com>
2025-10-10 02:33:26 +0300

    feat: add the login function

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -9,6 +9,9 @@ from models.enums.ResponseSignal import ResponseSignal
 from models.enums.UserEnums import AccountStatus
 from fastapi.responses import JSONResponse
 import bcrypt
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordRequestForm
+from src.routes.auth.authentication import create_access_token
 
 auth_router = APIRouter(
     prefix="/auth",
@@ -42,3 +45,25 @@ async def register_user(user_data: dict, db_client=Depends(get_Settings().MONGOD
             "signal": ResponseSignal.USER_REGISTERED_SUCCESSFULLY.value
         }
     )
+
+
+@auth_router.post("/token")
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db_client=Depends(get_Settings().MONGODB_URL)
+):
+    user_model = await UserModel.create_instance(db_client)
+    user = await user_model.get_user_by_email(form_data.username)  # username field is actually email
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+
+    if not bcrypt.checkpw(form_data.password.encode("utf-8"), user.hashPassword.encode("utf-8")):
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+
+    if user.accountStatus != AccountStatus.ACTIVE:
+        raise HTTPException(status_code=403, detail="Account not active")
+
+    # create token payload
+    access_token = create_access_token(data={"sub": str(user.id)})
+    return {"access_token": access_token, "token_type": "bearer"}
+
