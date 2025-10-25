959de83a4c93120d4e12e00532623a6c2b6bea78
mohamed-yehia311 <mmoa311311@gmail.com>
2025-10-25 17:25:25 +0300

    refactor: remove unused recommendation schemas from models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,6 +3,3 @@ from .schemas import facebookSchemas
 from .schemas import CompetitorRequest
 from .schemas import CompetitorSummary
 from src.controllers.analytics import AnalyticsController
-from .schemas.Recommendation import Recommendation
-from .schemas.RecommendationsResponse import RecommendationsResponse
-from .schemas.RecommendationsRequest import RecommendationsRequest
