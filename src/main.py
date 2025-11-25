82c8d623cf977919ee4dc9603528c981ad3f6fea
Abdelrahman66880 <abdofathi317@gmail.com>
2025-11-25 05:53:17 +0200

    lolo

--- src/main.py
+++ src/main.py
@@ -8,13 +8,12 @@ app = FastAPI()
 @app.on_event("startup")
 async def startup_db_client():
     settings = get_Settings()
-    # app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
-    # app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
-    # app.mongo_conn.close()
-    pass
+    app.mongo_conn.close()
 
 app.include_router(facebook.facebook_router)
 app.include_router(drafts.draft_router)
